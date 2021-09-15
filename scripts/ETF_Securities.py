67fc68bbadf5da2f36a74da40ecf677b95b320e2
Tikaraj Ghising <tghising>
2021-09-16 09:51:19 +1000

    Extraction of holdings modified

--- scripts/ETF_Securities.py
+++ scripts/ETF_Securities.py
@@ -32,15 +32,32 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
+# CONSTANTS Configurations
+INVESTMENT_PRODUCTS_LIST = 'ETF Securities List.xlsx'
+BASE_URL = 'https://www.etfsecurities.com.au'
+OUTPUT_DIR = r'etfs'
+LOGS_DIR = r'etfs'
 
-# In[2]:
+TIME_OUT = 30  # timeout for all web requests, else may hang
+
+ALL_ETF_SECURITIES_FILE = f'{OUTPUT_DIR}\\All ETF Securities.xlsx'
+
+# Note that this also determines the column order in the Excel file.
+COLUMN_TO_DISPLAY = ['Issuer', 'etf ticker', 'Security Ticker', 'Country Code', 'Security Name', 'Weight %',
+                     'Market Value', 'Rate', 'Maturity date', 'Sector', 'Country']
+
+COLUMN_RE_MAPPING = {'Component Name': 'Security Name',
+                  'Weight': 'Weight %',
+                  'Market Value (Base CCY)': 'Market Value'
+                  # 'Bloomberg Ticker':'Security Ticker'
+                  }
 
 
 def create_dir(dirName):
     """ Create if it doesn't exist """
     import os
     try:
-        os.mkdir(dirName)       # Create target Directory
+        os.makedirs(dirName)  # Create target Directory
     except FileExistsError:
         pass
 
@@ -53,8 +70,8 @@ def writelog(msg):
 
 
 def keep_list(k, all):
-    """ Given a list of columns to keep and  
-    a list of columns in the df, 
+    """ Given a list of columns to keep and
+    a list of columns in the df,
     Return a list of the overlap in the order of the keep list """
     intersection = []
     for c in k:
@@ -63,113 +80,111 @@ def keep_list(k, all):
     return intersection
 
 
-
 def etf_get_holdings(fund, link):
-    """ ETF Securities """
-    # ---------- constants ----------
-    col_rename = {'Component Name':'Security Name',
-                  'Weight':'Weight %',
-                  'Market Value (Base CCY)':'Market Value',
-                  # 'Bloomberg Ticker':'Security Ticker'
-                 }
-    base_url = 'https://www.etfsecurities.com.au'
     try:
-        r = requests.get(link, timeout=t_out)           # pull the containing page
+        response = requests.get(link, timeout=TIME_OUT)  # pull the containing page
     except  Exception as e:
         writelog(f'{fund}\tCould not get the containing page {link}: {e}')
         return pd.DataFrame()
-    soup = BeautifulSoup(r.content, 'lxml')     # parse
+    soup = BeautifulSoup(response.content, 'lxml')  # parse
     tag = soup.find(href=re.compile('\.xlsx'))  # find the tag with the link
     if not tag:
         writelog(f'{fund}\tCould not find the spreadsheet link in\t{link}')
         return pd.DataFrame()
-    
-    file_url = base_url + tag['href']           # add the relative link
+
+    file_url = BASE_URL + tag['href']  # add the relative link
+
     try:
-        r = requests.get(file_url, timeout=t_out)                  # get the file
+        res_data = requests.get(file_url, timeout=TIME_OUT)  # get the file
     except  Exception as e:
         writelog(f'{fund}\tCould not get the spreadsheet: {e}')
         return pd.DataFrame()
-    df = pd.read_excel(r.content, skiprows=18)
-    df = df.dropna(thresh=5)            # to drop the total row and others mostly null
+
+    df = pd.read_excel(res_data.content, skiprows=18)
+    df = df.dropna(thresh=5)  # to drop the total row and others mostly null
 
     # Splitting Bloomberg Ticker column into "Security Ticker", "Country Code" columns
     if "Bloomberg Ticker" in df.columns:
         df[['Security Ticker', 'Country Code', 'Security Type']] = df['Bloomberg Ticker'].str.split(' ', 0, expand=True)
 
     # if we find any source columns in the rename dict, rename them
-    for col in col_rename:
+    for col in COLUMN_RE_MAPPING:
         if col in list(df):
-            df.rename(columns={col:col_rename[col]}, inplace=True)
-    
+            df.rename(columns = {col: COLUMN_RE_MAPPING[col]}, inplace=True)
+
     if save_individual_files:
-        save_file = f'{wkg_dir}\\{start.strftime("%Y%m%d")}_{fund}.xlsx'
-        df.to_excel(save_file, sheet_name=fund, index=False, freeze_panes=(1,0))
+        save_file = f'{OUTPUT_DIR}\\{start.strftime("%Y%m%d")}_{fund}.xlsx'
+        df.to_excel(save_file, sheet_name = fund, index = False, freeze_panes = (1, 0))
 
     df['etf ticker'] = fund
-    keep = keep_list(col_keep, list(df))
+    keep = keep_list(COLUMN_TO_DISPLAY, list(df))
     return df[keep]
 
 
 
-# ---------- set up constants ---------- #
-wkg_dir = 'ETF Securities Py'
-create_dir(wkg_dir)
-host = 'https://www3.vanguard.com.au'
-path = '/personal/products/funds.json'
-url = host + path
+# ============================
+#      START SCRIPT FROM HERE
+# ============================
+print("======================================================================")
+print("                 ETF Securities - Holdings EXTRACT: STARTED           ")
+print("======================================================================")
+
+create_dir(OUTPUT_DIR)
+create_dir(LOGS_DIR)
 start = datetime.now()
 start_day = start.strftime("%Y%m%d")
 save_individual_files = False
-t_out = 30      # timeout for all web requests, else may hang
-log_dir = wkg_dir
 logfile = f'{start_day}_{os.path.basename(__file__).split(".")[0]}.log'
-lf = open(f'{log_dir}\\{logfile}', 'a')
-lf.write('\n'+'-'*75)
+
+lf = open(f'{LOGS_DIR}\\{logfile}', 'a')
+lf.write('\n' + '-' * 75 + '\n')
 # After renaming columns, keep only these ones in the final file
 # Note that this also determines the column order in the Excel file.
-col_keep = ['Issuer','etf ticker','Security Ticker','Country Code','Security Name','Weight %','Market Value', 'Rate','Maturity date','Sector','Country']
-all_funds = pd.DataFrame()  # empty data frame to add to
-list_file = 'ETF Securities List.xlsx'
-total_file = f'{wkg_dir}\\ETF_Securities.xlsx'
-fund_list = pd.read_excel(list_file, engine="openpyxl", )
-fund_list.fillna('', inplace = True)    # convert NA to empty string (from float)
+all_funds = pd.DataFrame()  # empty output frame to add to
+
+fund_list = pd.read_excel(INVESTMENT_PRODUCTS_LIST, engine="openpyxl", )
+fund_list.fillna('', inplace=True)  # convert NA to empty string (from float)
 
 # ---------- Main Loop ---------- #
 
 for i in range(len(fund_list)):
     # take care - headings are case sensitive
-    fund = fund_list.loc[i,'ASX Code']
-    #etf_cat = fund_list.loc[i,'ETF Category']
-    link = fund_list.loc[i,'Link']
-    issuer = fund_list.loc[i,'Issuer']
+    fund = fund_list.loc[i, 'ASX Code']
+    # etf_cat = fund_list.loc[i,'ETF Category']
+    link = fund_list.loc[i, 'Link']
+    issuer = fund_list.loc[i, 'Issuer']
     writelog(f'{fund}\t{issuer}\tStarting...')
-    if len(link) > 4:       # skip NA
+    if len(link) > 4:  # skip NA
         if issuer.lower().startswith('etf'):
             holdings = etf_get_holdings(fund, link)
         else:
             writelog(f'{fund}\t{issuer}\tDid not recognise this issuer')
-        
+
         # if the function returned an empty dataframe, skip
         if len(holdings) == 0:
             writelog(f'{fund}\t{issuer}\tFailed to get holdings')
         else:
             # writelog(f'{fund}\t{issuer}\tAdding {len(holdings)} rows')
-            holdings['ETF Category'] = fund_list.loc[i,'ETF Category']
-            holdings['Issuer'] = fund_list.loc[i,'Issuer']
+            holdings['ETF Category'] = fund_list.loc[i, 'ETF Category']
+            holdings['Issuer'] = fund_list.loc[i, 'Issuer']
             all_funds = all_funds.append(holdings)
-            # all_funds.to_excel(total_file, sheet_name='ETF', index=False, freeze_panes=(1,0))
+            # all_funds.to_excel(ALL_ETF_SECURITIES_FILE, sheet_name='ETF', index=False, freeze_panes=(1,0))
             # write out the combined DF every time in case of crash
     else:
         writelog(f'{fund}\t{issuer}\tSKIPPING, not a valid link')
 
 # Saving all at the end - riskier
-all_funds.to_excel(total_file, sheet_name='ETF', index=False, freeze_panes=(1,0))
-writelog(f'Saved the combined file {total_file} size {all_funds.shape}')
+all_funds.to_excel(ALL_ETF_SECURITIES_FILE, sheet_name='ETFS', index=False, freeze_panes=(1, 0))
+print("\n")
+writelog(f'Saved the combined file {ALL_ETF_SECURITIES_FILE} size {all_funds.shape}')
 
 # ------ Print the time taken and Exit ----------
 end = datetime.now()
 time_taken = end - start
 # writelog(f'Finished at {end.strftime("%H:%M:%S")}')
-m, s = divmod(time_taken.seconds,60)
+m, s = divmod(time_taken.seconds, 60)
 writelog(f'This took {m} minutes, {s} seconds for {len(fund_list)} funds')
+
+print("\n***********************************************************************")
+print("                  ETF Securities - Holdings EXTRACT : COMPLETED          ")
+print("***********************************************************************")
--- scripts/State_Street.py
+++ scripts/State_Street.py
@@ -32,11 +32,30 @@ import requests
 import pandas as pd
 
 
+# CONSTANTS Configurations
+INVESTMENT_PRODUCTS_LIST = 'State Street List.xlsx'
+OUTPUT_DIR = r'state-street'
+LOGS_DIR = r'state-street'
+
+TIME_OUT = 30  # timeout for all web requests, else may hang
+
+ALL_STATE_STREET_SECURITIES_FILE = f'{OUTPUT_DIR}\\All ETF Securities.xlsx'
+
+# Note that this also determines the column order in the Excel file.
+COLUMN_TO_DISPLAY = ['Issuer', 'etf ticker', 'Security Ticker', 'Country Code', 'Security Name', 'Weight %',
+                     'Market Value', 'Rate', 'Maturity date', 'Sector', 'Country', 'Number of Shares', 'Local Price']
+
+COLUMN_RE_MAPPING = {'Name': 'Security Name',
+                     'Weight (%)': 'Weight %'
+                     # 'Ticker': 'Security Ticker'
+                     }
+
+
 def create_dir(dirName):
     """ Create if it doesn't exist """
     import os
     try:
-        os.mkdir(dirName)  # Create target Directory
+        os.makedirs(dirName)  # Create target Directory
     except FileExistsError:
         pass
 
@@ -49,8 +68,8 @@ def writelog(msg):
 
 
 def keep_list(k, all):
-    """ Given a list of columns to keep and  
-    a list of columns in the df, 
+    """ Given a list of columns to keep and
+    a list of columns in the df,
     Return a list of the overlap in the order of the keep list """
     intersection = []
     for c in k:
@@ -62,18 +81,17 @@ def keep_list(k, all):
 def ss_get_holdings(fund, link):
     """ State Street v2.0
     not using the link at all now, just the fund """
-    col_rename = {'Name': 'Security Name',
-                  'Weight (%)': 'Weight %'
-                  # 'Ticker': 'Security Ticker'
-                  }
+
     file_url = f'https://www.ssga.com/au/en_gb/individual/etfs/library-content/products/fund-data/etfs/apac/holdings-daily-au-en-{fund.lower()}.xlsx'
     try:
-        r = requests.get(file_url, timeout=t_out)  # get the file
+        res = requests.get(file_url, timeout=TIME_OUT)  # get the file
     except  Exception as e:
         writelog(f'{fund}\tCould not get the spreadsheet {file_url}: {e}')
         return pd.DataFrame()
-    df = pd.read_excel(r.content, skiprows=4)
+
+    df = pd.read_excel(res.content, skiprows=4)
     df = df.dropna(thresh=5)  # to drop the total row and others mostly null
+
     # Splitting Bloomberg Ticker column into "Security Ticker", "Country Code" columns
     if "Ticker" in df.columns:
         is_contains_space = df['Ticker'].str.contains("-").any()
@@ -83,41 +101,42 @@ def ss_get_holdings(fund, link):
             df['Security Ticker'] = df['Ticker']
 
     # if we find any source columns in the rename dict, rename them
-    for col in col_rename:
+    for col in COLUMN_RE_MAPPING:
         if col in list(df):
-            df.rename(columns={col: col_rename[col]}, inplace=True)
+            df.rename(columns={col: COLUMN_RE_MAPPING[col]}, inplace=True)
     if save_individual_files:
-        save_file = f'{wkg_dir}\\{start.strftime("%Y%m%d")}_{fund}.xlsx'
+        save_file = f'{OUTPUT_DIR}\\{start.strftime("%Y%m%d")}_{fund}.xlsx'
         df.to_excel(save_file, sheet_name=fund, index=False, freeze_panes=(1, 0))
+
     df['Issuer'] = 'State Street'
     df['etf ticker'] = fund
-    keep = keep_list(col_keep, list(df))
+    keep = keep_list(COLUMN_TO_DISPLAY, list(df))
     return df[keep]
 
 
-# ---------- set up constants ---------- #
-wkg_dir = 'State Street Py'
-create_dir(wkg_dir)
-host = 'https://www3.vanguard.com.au'
-path = '/personal/products/funds.json'
-url = host + path
+# ============================
+#      START SCRIPT FROM HERE
+# ============================
+print("======================================================================")
+print("                 State Street - Holdings EXTRACT: STARTED             ")
+print("======================================================================")
+
+create_dir(OUTPUT_DIR)
+create_dir(LOGS_DIR)
 start = datetime.now()
 start_day = start.strftime("%Y%m%d")
 save_individual_files = False
-t_out = 30  # timeout for all web requests, else may hang
-log_dir = wkg_dir
+
 logfile = f'{start_day}_{os.path.basename(__file__).split(".")[0]}.log'
-lf = open(f'{log_dir}\\{logfile}', 'a')
-lf.write('\n' + '-' * 75)
+lf = open(f'{LOGS_DIR}\\{logfile}', 'a')
+lf.write('\n' + '-' * 75 + '\n')
 # After renaming columns, keep only these ones in the final file
 # Note that this also determines the column order in the Excel file.
 col_keep = ['Issuer', 'etf ticker', 'Security Ticker', 'Country Code', 'Security Name', 'Weight %', 'Market Value',
             'Rate', 'Maturity date', 'Sector', 'Country', 'Number of Shares', 'Local Price']
-all_funds = pd.DataFrame()  # empty data frame to add to
-list_file = 'fund list test.xlsx'
-list_file = 'State Street List.xlsx'
-total_file = f'{wkg_dir}\\State_Street.xlsx'
-fund_list = pd.read_excel(list_file, engine="openpyxl", )
+all_funds = pd.DataFrame()  # empty output frame to add to
+
+fund_list = pd.read_excel(INVESTMENT_PRODUCTS_LIST, engine="openpyxl", )
 fund_list.fillna('', inplace=True)  # convert NA to empty string (from float)
 
 # ---------- Main Loop ---------- #
@@ -144,14 +163,15 @@ for i in range(len(fund_list)):
             holdings['ETF Category'] = fund_list.loc[i, 'ETF Category']
             holdings['Issuer'] = fund_list.loc[i, 'Issuer']
             all_funds = all_funds.append(holdings)
-            # all_funds.to_excel(total_file, sheet_name='ETF', index=False, freeze_panes=(1,0))
+            # all_funds.to_excel(ALL_STATE_STREET_SECURITIES_FILE, sheet_name='ETF', index=False, freeze_panes=(1,0))
             # write out the combined DF every time in case of crash
     else:
         writelog(f'{fund}\t{issuer}\tSKIPPING, not a valid link')
 
 # Saving all at the end - riskier
-all_funds.to_excel(total_file, sheet_name='ETF', index=False, freeze_panes=(1, 0))
-writelog(f'Saved the combined file {total_file} size {all_funds.shape}')
+all_funds.to_excel(ALL_STATE_STREET_SECURITIES_FILE, sheet_name='ETF', index=False, freeze_panes=(1, 0))
+print("\n")
+writelog(f'Saved the combined file {ALL_STATE_STREET_SECURITIES_FILE} size {all_funds.shape}')
 
 # ------ Print the time taken and Exit ----------
 end = datetime.now()
@@ -159,3 +179,7 @@ time_taken = end - start
 # writelog(f'Finished at {end.strftime("%H:%M:%S")}')
 m, s = divmod(time_taken.seconds, 60)
 writelog(f'This took {m} minutes, {s} seconds for {len(fund_list)} funds')
+
+print("\n***********************************************************************")
+print("                  State Street - Holdings EXTRACT : COMPLETED            ")
+print("***********************************************************************")
